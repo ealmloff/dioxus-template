193e83749113f1c7c7f0131ca09fe40bd8c8e00f
Jonathan Kelley <jkelleyrtp@gmail.com>
2024-11-16 12:37:03 -0500

    Update main.rs - dioxus-sdk to sdk

--- src/main.rs
+++ src/main.rs
@@ -57,7 +57,7 @@ fn app() -> Element {
             a { href: "https://dioxuslabs.com/learn/0.6/", "📚 Learn Dioxus" }
             a { href: "https://dioxuslabs.com/awesome", "🚀 Awesome Dioxus" }
             a { href: "https://github.com/dioxus-community/", "📡 Community Libraries" }
-            a { href: "https://github.com/DioxusLabs/dioxus-sdk", "⚙️ Dioxus Development Kit" }
+            a { href: "https://github.com/DioxusLabs/sdk", "⚙️ Dioxus Development Kit" }
             a { href: "https://marketplace.visualstudio.com/items?itemName=DioxusLabs.dioxus",
                 "💫 VSCode Extension"
             }
