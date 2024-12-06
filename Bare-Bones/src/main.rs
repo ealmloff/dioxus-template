51dac38058b54b1fdd8dda09bba51fa01d1ee97c
Jonathan Kelley <jkelleyrtp@gmail.com>
2024-12-06 13:41:32 -0800

    syntax tweaks

--- Bare-Bones/src/main.rs
+++ Bare-Bones/src/main.rs
@@ -26,7 +26,6 @@ fn main() {
 #[component]
 fn App() -> Element {
     // Build cool things ✌️
-
     rsx! {
         // Global app resources
         document::Link { rel: "icon", href: FAVICON }
@@ -34,7 +33,6 @@ fn App() -> Element {
         {% if is_tailwind -%}
         document::Link { rel: "stylesheet", href: TAILWIND_CSS }
         {%- endif %}
-
         {% if is_router -%}
         Router::<Route> {}
         {%- else -%}
@@ -53,12 +51,12 @@ pub fn Hero() -> Element {
             id: "hero",
             img { src: HEADER_SVG, id: "header" }
             div { id: "links",
-                a { target: "_blank", href: "https://dioxuslabs.com/learn/0.6/", "📚 Learn Dioxus" }
-                a { target: "_blank", href: "https://dioxuslabs.com/awesome", "🚀 Awesome Dioxus" }
-                a { target: "_blank", href: "https://github.com/dioxus-community/", "📡 Community Libraries" }
-                a { target: "_blank", href: "https://github.com/DioxusLabs/sdk", "⚙️ Dioxus Development Kit" }
-                a { target: "_blank", href: "https://marketplace.visualstudio.com/items?itemName=DioxusLabs.dioxus", "💫 VSCode Extension" }
-                a { target: "_blank", href: "https://discord.gg/XgGxMSkvUM", "👋 Community Discord" }
+                a { href: "https://dioxuslabs.com/learn/0.6/", "📚 Learn Dioxus" }
+                a { href: "https://dioxuslabs.com/awesome", "🚀 Awesome Dioxus" }
+                a { href: "https://github.com/dioxus-community/", "📡 Community Libraries" }
+                a { href: "https://github.com/DioxusLabs/sdk", "⚙️ Dioxus Development Kit" }
+                a { href: "https://marketplace.visualstudio.com/items?itemName=DioxusLabs.dioxus", "💫 VSCode Extension" }
+                a { href: "https://discord.gg/XgGxMSkvUM", "👋 Community Discord" }
             }
         }
     }
--- Workspace/ui/src/hero.rs
+++ Workspace/ui/src/hero.rs
@@ -12,12 +12,12 @@ pub fn Hero() -> Element {
             id: "hero",
             img { src: HEADER_SVG, id: "header" }
             div { id: "links",
-                a { target: "_blank", href: "https://dioxuslabs.com/learn/0.6/", "📚 Learn Dioxus" }
-                a { target: "_blank", href: "https://dioxuslabs.com/awesome", "🚀 Awesome Dioxus" }
-                a { target: "_blank", href: "https://github.com/dioxus-community/", "📡 Community Libraries" }
-                a { target: "_blank", href: "https://github.com/DioxusLabs/sdk", "⚙️ Dioxus Development Kit" }
-                a { target: "_blank", href: "https://marketplace.visualstudio.com/items?itemName=DioxusLabs.dioxus", "💫 VSCode Extension" }
-                a { target: "_blank", href: "https://discord.gg/XgGxMSkvUM", "👋 Community Discord" }
+                a { href: "https://dioxuslabs.com/learn/0.6/", "📚 Learn Dioxus" }
+                a { href: "https://dioxuslabs.com/awesome", "🚀 Awesome Dioxus" }
+                a { href: "https://github.com/dioxus-community/", "📡 Community Libraries" }
+                a { href: "https://github.com/DioxusLabs/sdk", "⚙️ Dioxus Development Kit" }
+                a { href: "https://marketplace.visualstudio.com/items?itemName=DioxusLabs.dioxus", "💫 VSCode Extension" }
+                a { href: "https://discord.gg/XgGxMSkvUM", "👋 Community Discord" }
             }
         }
     }
