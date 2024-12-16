230d4d8d9434a5abf5c40f0e83156b74591324a5
Evan Almloff <evanalmloff@gmail.com>
2024-12-16 14:30:07 -0600

    document features and dependencies in all Cargo.tomls

--- Workspace/server/src/lib.rs
+++ Workspace/server/src/lib.rs
@@ -1,8 +1,16 @@
 //! This crate contains all shared fullstack server functions.
 use dioxus::prelude::*;
 
-/// Echo the user input on the server.
-#[server(Echo)]
-pub async fn echo(input: String) -> Result<String, ServerFnError> {
+// Server functions let us define public APIs on the server that can be called like a normal async function from the client.
+// Each server function needs to be annotated with the `#[server]` attribute, accept and return serializable types, and return
+// a `Result` with the error type [`ServerFnError`].
+//
+// When the server function is called from the client, it will just serialize the arguments, call the API, and deserialize the
+// response.
+#[server]
+async fn echo_server(input: String) -> Result<String, ServerFnError> {
+    // The body of server function like this comment are only included on the server. If you have any server-only logic like
+    // database queries, you can put it here. Any imports for the server function should either be imported inside the function
+    // or imported under a `#[cfg(feature = "server")]` block.
     Ok(input)
 }
