bc296c25d153ee30d9161736fb162138220201d2
Evan Almloff <evanalmloff@gmail.com>
2024-12-16 11:38:22 -0600

    document the Jumpstart template

--- Jumpstart/src/components/mod.rs
+++ Jumpstart/src/components/mod.rs
@@ -1,10 +1,9 @@
+//! The components module contains all shared components for our app. Components are the building blocks of dioxus apps.
+//! They can be used to defined common UI elements like buttons, forms, and modals. In this template, we define a Hero
+//! component {% if is_fullstack -%} and an Echo component for fullstack apps {%- endif %} to be used in our app.
+
 mod hero;
 pub use hero::Hero;
-
-{% if is_router -%}
-mod navbar;
-pub use navbar::Navbar;
-{%- endif %}
 
 {% if is_fullstack -%}
 mod echo;
